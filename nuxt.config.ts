88c458c2137f249c7ada5092e7a5a9313b5d3e8b
fenghang <hang.feng@nx-engine.com>
2025-01-02 15:00:11 +0800

    feat: update Dockerfile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -275,6 +275,7 @@ export default defineNuxtConfig({
         { name: 'twitter:card', content: 'summary_large_image' },
       ],
     },
+    baseURL: '/elk/'
   },
 
   // eslint-disable-next-line ts/ban-ts-comment
